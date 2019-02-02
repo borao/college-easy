513779de2199a36fbcc8237bc9c28c9aa4bb8cf6
Bora Oztekin <borao@users.noreply.github.com>
2019-02-02 11:05:32 -0600

    Add manage file and fix environment variable accessors

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config.from_object(os.environ['APP_SETTINGS'])
+app.config.from_object(os.environ.get('APP_SETTINGS'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+from models import College
 
 @app.route('/')
 def hello_world():
