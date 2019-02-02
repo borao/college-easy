a89b734487aa78dea0d38625e5ab4227c149c8ba
Bora Oztekin <borao@users.noreply.github.com>
2019-02-02 13:21:03 -0600

    WIP Add view to add college

--- app.py
+++ app.py
@@ -10,13 +10,40 @@ db = SQLAlchemy(app)
 
 from models import College
 
+
 @app.route('/')
-def hello_world():
+def index():
     try:
         colleges = College.query.all()
         return render_template("index.html", colleges=colleges)
     except Exception as e:
-        return (str(e))
+        return str(e)
+
+
+@app.route('/add_college')
+def add_college():
+    name = request.args.get('name')
+    app_deadline = request.args.get('app_deadline')
+    rec_deadline = request.args.get('rec_deadline')
+    num_essays = request.args.get('num_essays')
+    midyear_report = request.args.get('midyear_report')
+    acceptance_rate = request.args.get('acceptance_rate')
+    platform = request.args.get('platform')
+    try:
+        college = College(
+            name=name,
+            app_deadline=app_deadline,
+            rec_deadline=rec_deadline,
+            num_essays=num_essays,
+            midyear_report=midyear_report,
+            acceptance_rate=acceptance_rate,
+            platform=platform,
+        )
+        db.session.add(college)
+        db.session.commit()
+        return "College added. college id={}".format(college.id)
+    except Exception as e:
+        return str(e)
 
 
 if __name__ == '__main__':
