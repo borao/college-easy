9ec9524f15c2212abb21d6c9cce4a4391d3a661f
Bora Oztekin <borao@users.noreply.github.com>
2019-02-02 10:11:02 -0600

    Add database configuration

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 @app.route('/')
