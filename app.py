3c87764a5e47d540fff46f1f1dc5516678bdb79e
Bora Oztekin <borao@users.noreply.github.com>
2019-02-02 14:05:29 -0600

    Redirect to list of colleges if add college view is accessed without a post request

--- app.py
+++ app.py
@@ -20,30 +20,33 @@ def index():
         return str(e)
 
 
-@app.route('/add_college', methods=['POST'])
+@app.route('/add_college', methods=['GET', 'POST'])
 def add_college():
-    name = request.args.get('name')
-    app_deadline = request.args.get('app_deadline')
-    rec_deadline = request.args.get('rec_deadline')
-    num_essays = request.args.get('num_essays')
-    midyear_report = request.args.get('midyear_report')
-    acceptance_rate = request.args.get('acceptance_rate')
-    platform = request.args.get('platform')
-    try:
-        college = College(
-            name=name,
-            app_deadline=app_deadline,
-            rec_deadline=rec_deadline,
-            num_essays=num_essays,
-            midyear_report=midyear_report,
-            acceptance_rate=acceptance_rate,
-            platform=platform,
-        )
-        db.session.add(college)
-        db.session.commit()
-        return redirect('/')
-    except Exception as e:
-        return str(e)
+    if request.method == "POST":
+        name = request.args.get('name')
+        app_deadline = request.args.get('app_deadline')
+        rec_deadline = request.args.get('rec_deadline')
+        num_essays = request.args.get('num_essays')
+        midyear_report = request.args.get('midyear_report')
+        acceptance_rate = request.args.get('acceptance_rate')
+        platform = request.args.get('platform')
+        try:
+            college = College(
+                name=name,
+                app_deadline=app_deadline,
+                rec_deadline=rec_deadline,
+                num_essays=num_essays,
+                midyear_report=midyear_report,
+                acceptance_rate=acceptance_rate,
+                platform=platform,
+            )
+            db.session.add(college)
+            db.session.commit()
+            return redirect('/')
+        except Exception as e:
+            return str(e)
+    else:
+        return render_template("add_college.html")
 
 
 if __name__ == '__main__':
