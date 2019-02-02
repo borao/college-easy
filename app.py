0aef6c0ba30759d153a83275aaacbbb4d19cfad7
Bora Oztekin <borao@users.noreply.github.com>
2019-02-02 16:45:35 -0600

    Edit college based on URL arguments

--- app.py
+++ app.py
@@ -51,15 +51,24 @@ def add_college():
 
 @app.route('/edit_college', methods=['GET', 'POST'])
 def edit_college():
+    college = College.query.filter_by(name=request.args.get('name')).one()
     if request.method == "POST":
-        pass
+        college.name = request.form.get('name')
+        college.app_deadline = request.form.get('app_deadline')
+        college.rec_deadline = request.form.get('rec_deadline')
+        college.num_essays = request.form.get('num_essay')
+        college.midyear_report = request.form.get('midyear_report')
+        college.acceptance_rate = request.form.get('acceptance_rate')
+        college.platform = request.form.get('platform')
+        College.commit()
+        return redirect('/')
     else:
         try:
-            college = College.query.filter_by(name="Texas A&M University").one()
             is_editing = True
             return render_template("college.html", college=college, is_editing=is_editing)
         except Exception as e:
             return str(e)
+
 
 if __name__ == '__main__':
     app.run()
